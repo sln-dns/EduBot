088bc3fb79179057a4a58552ec9b9728844f2381
Denis Silin <Silin.dns@gmail.com>
2021-05-21 19:13:31 +0300

    убрал словарь с планетами

--- bot.py
+++ bot.py
@@ -26,30 +26,21 @@ def talk_to_me(update, context):
     update.message.reply_text(text)
 
 def planet(update, context):
-    list_planet = {'sun': 'Sun', 'moon': 'Moon', 'mercury': 'Mercury', 
-       'venus': 'Venus', 'mars': 'Mars', 'phobos': 'Phobos', 'deimos': 'Deimos', 
-       'jupiter': 'Jupiter', 'io': 'Io', 'europa': 'Europa', 'ganymede': 'Ganymede',
-       'callisto': 'Callisto', 'saturn': 'Saturn', 'mimas': 'Mimas', 'enceladus': 'Enceladus', 
-       'tethys': 'Tethys', 'dione': 'Dione', 'rhea': 'Rhea', 'titan': 'Titan', 
-       'hyperion': 'Hyperion', 'iapetus': 'Iapetus', 'uranus': 'Uranus', 'ariel': 'Ariel', 
-       'umbriel': 'Umbriel', 'titania': 'Titania', 'oberon': 'Oberon', 'miranda': 'Miranda', 
-       'neptune': 'Neptune','pluto': 'Pluto'
-    }
-   
+       
     text  = update.message.text
     text = text.lower()
     text = text.split()
     
     try:
         name_planet = text[1]
-        name_planet = list_planet.get(name_planet)
+        name_planet = name_planet.title()        
         planet = getattr(ephem, name_planet)(date.today())
         constellation = ephem.constellation(planet)
         text = f'Сегодня {name_planet} в созвездии {constellation}'
         update.message.reply_text(text)
         
         
-    except TypeError: 
+    except (TypeError, AttributeError): 
         text = 'не припомню такой планеты. Попробуй ещё раз' 
         update.message.reply_text(text)
     except IndexError:
@@ -64,8 +55,8 @@ def main():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(CommandHandler('planet', planet))
-    dp.add_handler(MessageHandler(Filters.text, planet))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    #dp.add_handler(MessageHandler(Filters.text, planet))
+    #dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     
 
 
