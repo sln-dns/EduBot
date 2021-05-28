79bdaf78008b401a8514b959b0079ec3ff88b04a
Denis Silin <Silin.dns@gmail.com>
2021-05-28 21:06:57 +0300

    добавил созвездия в перевод, добаваил обработку ошибок

--- planet.py
+++ planet.py
@@ -1,31 +1,24 @@
 import ephem 
 from datetime import date
 import datetime
+import locale
+locale.setlocale(locale.LC_TIME, 'ru_RU.UTF8')
 import warnings 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 text = input('ввести /planet и планету    ')
-date_year = int(input('введите дату, интересующую вас. Введите год в формате ХХХХ  '))
-date_month = int(input('введите номер месяца '))
-date_date = int(input('введите число '))
 
 try:
-    date_interes = datetime.datetime(date_year, date_month, date_date)
-    #print(date_interes)
-    #print(date.today())
-    #mars = ephem.Mars(date_interes)
-    #constellation = ephem.constellation(mars)
-    #print(constellation)
+    date_year = int(input('введите дату, интересующую вас. Введите год в формате ХХХХ  '))
+    date_month = int(input('введите номер месяца '))
+    date_date = int(input('введите число '))
+    date_interes = datetime.date(date_year, date_month, date_date)
+    
 except ValueError:
     print('введена неверная дата, результат на сегодняшнюю дату')
     date_interes = date.today()
-    #mars = ephem.Mars(date_interes)
-    #constellation = ephem.constellation(mars)
-    #print(constellation)
-
-#def planet(update, context):
-
-   
+    
+ 
 text = text.lower()
 text = text.split()
 
@@ -37,11 +30,13 @@ dict_zodiack = {
         'Leo': 'Лев',
         'Virgo': 'Дева',
         'Libra': 'Весы',
-        'Scorpio': 'Скорпион',
+        'Scorpius': 'Скорпион',
         'Sagittarius': 'Стрелец',
         'Capricornus': 'Козерог',
         'Aquarius': 'Водолей',
-        'Pisces': 'Рыбы'
+        'Pisces': 'Рыбы',
+        'Bootes': 'Волопас',
+        'Ophiuchus': 'Змееносец'
         }
 
 dict_planet = {
@@ -63,11 +58,11 @@ try:
     constellation = ephem.constellation(planet)
     name_planet = dict_planet[name_planet]
     constellation = dict_zodiack[constellation[1]]
-    date_interes = date_interes.strftime("%d.%m.%y")
+    date_interes = date_interes.strftime("%d %B %Y")
     print(f'На дату {date_interes} небесное тело {name_planet}  находилось в созвездии {constellation}')
     #print(constellation[1])
 
-except TypeError: 
+except (TypeError, AttributeError): 
     print('не припомню такой планеты. Попробуй ещё раз')
 
 except IndexError:
